92dc65f69ff9e4dbe856392965dc8c2d62cda93d
Dania Kamrul Ariffin <ainuldania@gmail.com>
2020-10-26 14:17:23 +0800

    Added a line scene on Lecture 3.1

--- start/lecture3_1/app.js
+++ start/lecture3_1/app.js
@@ -52,7 +52,14 @@ class App{
     }
     
     initScene(){
-        
+        this.radius = 0.08;
+
+        this.room = new THREE.LineSegments(
+            new BoxLineGeometry(6,6,6,10,10,10),
+            new THREE.LineBasicMaterial({color:0x808080})
+        );
+        this.room.geometry.translate(0,3,0);
+        this.scene.add(this.room);
     }
     
     setupXR(){
