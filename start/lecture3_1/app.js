3585fcf7ab9c96f05ae4146545743e03398fbf3d
ainuldania <ainuldania@gmail.com>
2020-10-26 14:37:11 +0800

    Setup VR support with WebXR

--- start/lecture3_1/app.js
+++ start/lecture3_1/app.js
@@ -79,7 +79,8 @@ class App{
     }
     
     setupXR(){
-        
+        this.renderer.xr.enabled = true;
+        document.body.appendChild(VRButton.createButton(this.renderer));
     }
     
     resize(){
